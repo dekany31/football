752c4a985ba6635beb41de1deeea93511a07c023
dekany31 <66486271+dekany31@users.noreply.github.com>
2021-05-11 22:24:54 +0200

    Implementation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 import './App.css'
+import React, { useState, useEffect } from "react"
+import Team from "./components/Team"
 
 const App = () => {
 
+  const [isLoadingTeams, setIsLoadingTeams] = useState(false);
+  const [teams, setTeams] = useState([]);
+  const [search, setSearch] = useState("");
+
+
+  useEffect(() => {
+    let mounted = true;
+    setIsLoadingTeams(true);
+
+    fetch('api/teams')
+      .then(response => response.json())
+      .then(data => { if (mounted) setTeams(data); })
+      .catch(err => setTeams(null))
+      .finally(() => setIsLoadingTeams(false))
+    return () => mounted = false;
+  }, []);
+
   return (
     <div className="App">
-      Todo...
+      <h1>NBA teams - all star voting</h1>
+      <input type="text" onChange={(ev) => setSearch(ev.target.value)} />
+      {teams && teams.map((team) => <Team key={team.name} team={team} />)}
     </div>
   )
 }
